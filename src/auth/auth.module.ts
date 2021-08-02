b0b131afff91deece78f034cf34d338c770ee146
qbuser <arunkv.vit@gmail.com>
2021-08-02 11:46:12 +0530

    added jwt strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,13 +4,14 @@ import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [UsersModule,PassportModule,JwtModule.register({
     secret: 'secretKey',
-    signOptions: { expiresIn: '60s' },
+    signOptions: { expiresIn: '300s' },
   }),],
-  providers: [AuthService,LocalStrategy],
+  providers: [AuthService,LocalStrategy,JwtStrategy],
   exports: [AuthService]
 })
 export class AuthModule {}
